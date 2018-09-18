e373b9d87a62dba287161d5f86edf414942f1aaa
Nick Hsieh <mosabeker@gmail.com>
2018-09-19 02:15:10 +0800

    Add a solution with NaimishNet

--- models.py
+++ models.py
@@ -13,25 +13,82 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+
+       
+        # 1 input image channel (grayscale), 32 output channels/feature maps, 4x4 square convolution kernel   
+        #following the suggestion from NaimishNet but the size is 224x224 instead of 96x96
+        self.conv1 = nn.Conv2d(1, 32, 4) # (224 - 4)/1 + 1 = 221
+        self.conv1_bn = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(2,2) # (110, 110)
+        self.drop1 = nn.Dropout(0.1)
+
+        self.conv2 = nn.Conv2d(32, 64, 3) # (110 - 3)/1 + 1 = 108
+        self.conv2_bn = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(2,2) # (54, 54)
+        self.drop2 = nn.Dropout(0.2)
+
+        self.conv3 = nn.Conv2d(64, 128, 2) # (54 - 2)/ + 1 = 53
+        self.conv3_bn = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(2,2) # (26, 26)
+        self.drop3 = nn.Dropout(0.3)
+
+        self.conv4 = nn.Conv2d(128, 256, 1) # (26 - 1)/1 + 1 = 26
+        self.conv4_bn = nn.BatchNorm2d(256)
+        self.pool4 = nn.MaxPool2d(2,2) # (13, 13)
+        self.drop4 = nn.Dropout(0.4)
         
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv5 = nn.Conv2d(256, 384, 1) # (13 - 1)/1 + 1 = 13
+        self.conv5_bn = nn.BatchNorm2d(384)
+        self.pool5 = nn.MaxPool2d(2,2) # (6, 6)
+        self.drop5 = nn.Dropout(0.4)
         
+        self.conv6 = nn.Conv2d(384, 512, 1) # (6 - 1)/1 + 1 = 6
+        self.conv6_bn = nn.BatchNorm2d(512)
+        self.pool6 = nn.MaxPool2d(2,2) # (3, 3)
+        self.drop6 = nn.Dropout(0.4)
+               
+        self.fc1 = nn.Linear(512 * 3 * 3, 1000)
+        self.fc1bn = nn.BatchNorm1d(1000)
+        self.drop5 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(1000, 500)
+        self.fc2bn = nn.BatchNorm1d(500)
+        self.drop6 = nn.Dropout(0.6)
+        self.fc3 = nn.Linear(500, 136)
+
+
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
 
-        
+
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+
+        x = self.drop1(self.pool1(self.conv1_bn(F.elu(self.conv1(x)))))
+        x = self.drop2(self.pool2(self.conv2_bn(F.elu(self.conv2(x)))))
+        x = self.drop3(self.pool3(self.conv3_bn(F.elu(self.conv3(x)))))
+        x = self.drop4(self.pool4(self.conv4_bn(F.elu(self.conv4(x)))))
+        x = self.drop5(self.pool5(self.conv5_bn(F.elu(self.conv5(x)))))
+        x = self.drop6(self.pool6(self.conv6_bn(F.elu(self.conv6(x)))))
+    
+          
+        #Flatten
+        x = x.view(x.size(0), -1)
         
-        
+        #ELU
+        x = F.elu(self.fc1(x))
+        x = self.fc1bn(x)
+        x = self.drop5(x)
+        x = F.elu(self.fc2(x))
+        x = self.fc2bn(x)
+        x = self.drop6(x)
+        x = self.fc3(x)
+
         # a modified x, having gone through all the layers of your model, should be returned
         return x
+
+
